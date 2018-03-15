9a1afc0d5602398742aad974abacd78c7241b324
SteveSWD <steve.schmidt@wolfdenassociates.com>
2018-03-15 15:10:40 -0400

    improved templating

--- djangoApp/views.py
+++ djangoApp/views.py
@@ -8,13 +8,18 @@
 from django.http import HttpResponse
 import datetime
 from django import template
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello Django")
 
 def today_is(request):
     now = datetime.datetime.now()
-    t = template.loader.get_template('djangoApp/datetime.html')
-    c = {'now': now}
-    html = t.render(c)
-    return HttpResponse(html)
+    #t = template.loader.get_template('djangoApp/time.html')
+    #c = {'now': now}
+    #html = t.render(c)
+    #return HttpResponse(html)
+    return render (request, 'djangoApp/datetime.html', {
+        'now': now,
+        'template_name': 'djangoApp/nav.html'
+    })
